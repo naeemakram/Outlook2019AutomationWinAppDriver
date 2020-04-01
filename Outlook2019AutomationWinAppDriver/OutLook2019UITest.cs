f16527be5334a00dd6e0bb335de2691c990bdf20
Naeem Akram <naeem.akram.malik@gmail.com>
2020-04-01 20:07:44 +0500

    Creating a solution for UI automation of Microsoft OutLook 2019 in WinAppDriver
    Created a new project, wriiten code for following;
    Lauching Outlook
    Maximizing window
    Clicking Home tab
    Clicking "Search" icon
    Clicking "Has Attachments" icon

---
+++ Outlook2019AutomationWinAppDriver/OutLook2019UITest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
+
+namespace Outlook2019AutomationWinAppDriver
+{
+    [TestClass]
+    public class OutLook2019UITest
+    {
+        static TestContext mTestContext;
+        static WindowsDriver<WindowsElement> mSessionOutlook;
+        static WindowsDriver<WindowsElement> mSessionDesktop;
+        static AppiumOptions mOptionsOutlook;
+
+        static WebDriverWait mWaitOutlook;
+
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            mTestContext = testContext;
+            mOptionsOutlook = new AppiumOptions();
+            mOptionsOutlook.AddAdditionalCapability("app", "Outlook");
+            mOptionsOutlook.AddAdditionalCapability("ms:waitForAppLaunch", "15");
+            // ms:waitForAppLaunch
+
+            mSessionOutlook = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), mOptionsOutlook);
+
+            var optionsDesktop = new AppiumOptions();
+            optionsDesktop.AddAdditionalCapability("app", "Root");
+            mSessionDesktop = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), optionsDesktop);
+
+            mWaitOutlook = new WebDriverWait(mSessionOutlook, TimeSpan.FromSeconds(5));
+
+            mSessionOutlook.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void OpenOutLook()
+        {
+            var homeTab = mSessionOutlook.FindElementByName("Home");
+            mWaitOutlook.Until(x => homeTab.Displayed);
+            homeTab.Click();
+
+            var searchIcon = mSessionOutlook.FindElementByName("Submit Search");
+            searchIcon.Click();
+
+            var hasAttachmentsButton = mSessionOutlook.FindElementByName("Has Attachments");
+            mWaitOutlook.Until(x => hasAttachmentsButton.Displayed);
+            hasAttachmentsButton.Click();
+
+            //var messagesTable = mSessionOutlook.FindElementByName("Table View");
+            //messagesTable.Click();
+
+            //var allDataItems = messagesTable.FindElementsByTagName("DataItem");
+            //Debug.WriteLine($"***** Total data items: {allDataItems.Count}");
+            //foreach(var mail in allDataItems)
+            //{
+            //    Debug.WriteLine($"***** {mail.Text} \r\n {mail.GetAttribute("Name")}");
+            //}
+        }
+    }
+}
---
+++ Outlook2019AutomationWinAppDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Outlook2019AutomationWinAppDriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Outlook2019AutomationWinAppDriver")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7b156c0-f8ec-4f7e-8a25-840e58f2a800")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
